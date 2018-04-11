6fb0ed384b188dbc9ac6a9ff9a12d4afdb42a4a8
vuongpv <monkivn92@gmail.com>
2018-04-11 16:27:10 +0700

    add nerdlauncher project

---
+++ NerdLauncher/app/src/main/java/com/vdroid/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,14 @@
+package com.vdroid.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+
+
+public class NerdLauncherActivity extends SingleFragmentActivity
+{
+
+    @Override
+    protected Fragment createFragment()
+    {
+        return NerdLauncherFragment.newInstance();
+    }
+}
